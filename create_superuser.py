b50c5327ce7a53f406d3383a355361b75a1a41ba
Javier Lobato <davidlobato99@gmail.com>
2024-06-27 18:58:01 -0500

    admin password change

--- create_superuser.py
+++ create_superuser.py
@@ -10,7 +10,7 @@ User = get_user_model()
 
 username = 'admin'
 email = 'admin@example.com'
-password = 'adminpassword'
+password = 'admin123'
 
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username=username, email=email, password=password)
